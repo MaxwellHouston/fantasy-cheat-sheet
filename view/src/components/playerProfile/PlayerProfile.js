26e53b7f0ecf6363e39f2f6236c8fe0fa0fc9f47
MaxwellHouston <87088784+MaxwellHouston@users.noreply.github.com>
2022-11-22 14:24:48 -0500

    Added yearly stats table and stats by week table. Added stat type component and a divider. Created PlayerProfile.css

--- view/src/components/playerProfile/PlayerProfile.js
+++ view/src/components/playerProfile/PlayerProfile.js
@@ -1,15 +1,28 @@
-import { useState } from "react"
-import { PlayerBio } from "../PlayerBio/PlayerBio"
+import './PlayerProfile.css';
+import { useState } from "react";
+import { DataTable } from "../dataTable/DataTable";
+import { PlayerBio } from "../PlayerBio/PlayerBio";
+import { StatTypeMenu } from "../statTypeMenu/StatTypeMenu";
+import Divider from '@mui/material/Divider';
 
 
 export const PlayerProfile = () => {
 
+    const [statType, setStatType] = useState('passing');
+    const [seasonStats, setSeasonStats] = useState([]);
+    const [weeklyStats, setWeeklyStats] = useState([]);
 
+    const updateStatType = (newType) => {
+        setStatType(newType);
+    }
 
     return(
         <div className="player-profile-page">
             <PlayerBio />
-            
+            <StatTypeMenu statType={statType} updateStatType={updateStatType} />
+            <DataTable dataArray={seasonStats} rowIdOne={'id'} rowIdTwo={null} />
+            <Divider variant="middle" />
+            <DataTable dataArray={weeklyStats} rowIdOne={'id'} rowIdTwo={'week'} />
         </div>
     )
 }
