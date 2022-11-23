68147c247f48d340b3bd0643881a605a6df879d1
MaxwellHouston <87088784+MaxwellHouston@users.noreply.github.com>
2022-11-23 08:42:49 -0500

    Added espnAPI to make a get request to the ESPN API and pull data on a specific player. Then the data is stored in the state of the PlayerBio component

--- view/src/components/PlayerBio/PlayerBio.js
+++ view/src/components/PlayerBio/PlayerBio.js
@@ -1,6 +1,21 @@
+import { useEffect, useState, useCallback } from "react"
+import { fetchBio } from "../../utilities/espnAPI"
 
 
 export const PlayerBio = ({player}) => {
+
+    const [playerBio, setPlayerBio] = useState({})
+
+    const loadBio = useCallback(
+        async () => {
+            let data = await fetchBio(player);
+            setPlayerBio(data);
+        }, [player]
+    ) 
+
+    useEffect(() => {
+        loadBio();
+    },[loadBio])
 
     return(
         <div>
---
+++ view/src/utilities/espnAPI.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+export const fetchBio = async (playerName) => {
+    try {
+        let res = await axios('https://sports.core.api.espn.com/v3/sports/football/nfl/athletes', {
+        params: {
+            limit: 17500,
+        }
+        });
+        let filterPlayer = res.data.items.filter(player => player.fullName === playerName);
+        let playerObject = {
+            height: filterPlayer[0].displayHeight,
+            weight: filterPlayer[0].displayWeight,
+            age: filterPlayer[0].age,
+            jersey: filterPlayer[0].jersey,
+            imgSrc: `https://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/${filterPlayer[0].id}.png&w=426&h=320&cb=1`
+        }
+        return playerObject;
+    } catch (error) {
+        return error;
+    }
+}
+
